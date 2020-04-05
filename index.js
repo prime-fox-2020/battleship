fc07753800a8bff32e4ba22531f809d5955a2e3f
Mercen1 <nurhabib102@gmail.com>
2020-04-05 16:15:26 +0700

    Release 1 : Winning The Game

--- index.js
+++ index.js
@@ -84,6 +84,41 @@ class Armada{
             }
         }
     }
+
+    checkStatus(){
+        let temp5=0;
+        let temp4=0;
+        let temp3=0;
+        let temp2=0;
+        for (let i = 0; i < this.lautan.length; i++) {
+            for (let j = 0; j < this.lautan[i].length; j++) {
+                if(this.lautan[i][j]==5){
+                    temp5 ++
+                }else if(this.lautan[i][j]==4){
+                    temp4 ++
+                }else if(this.lautan[i][j]==3){
+                    temp3 ++
+                }else if(this.lautan[i][j]==2){
+                    temp2 ++
+                }
+            }
+        }
+        if(temp5<this.size[0]){
+            console.log(`Selamat!! Armada 'Aircraft Carier' telah Tertembak dan Tenggelam`)
+        }
+        if(temp4<this.size[1]){
+            console.log(`Selamat!! Armada 'Battleship' telah Tertembak dan Tenggelam`)
+        }
+        if(temp3<this.size[2]){
+            console.log(`Selamat!! Armada 'Crusier' telah Tertembak dan Tenggelam`)
+        }
+        if(temp2<this.size[3]){
+            console.log(`Selamat!! Armada 'Destroyer' telah Tertembak dan Tenggelam`)
+        }
+        if(temp5<this.size[0] && temp4<this.size[1] && temp3<this.size[2] && temp2<this.size[3]){
+            console.log(`Semua Kapal Telah TENGGELAM, Kamu Memenangkan GAME ini`)
+        }
+    }
 }
 
 
@@ -103,9 +138,6 @@ class Lautan{
         }
     }
 }
-
-
-
 
 let armada = [];
 
@@ -135,4 +167,8 @@ armadaPerang.tembakan(dataInputUser)
 console.log(`----Armada Sesudah-----`)
 console.log(`------Penyerangan------`)
 lautan.display()
+armadaPerang.checkStatus()
 
+
+
+// USER INPUT == $ node index.js A3 A4 A7 B1 B3 B8 C2 C5 C7 D1 D3 D7 E3 E7 E8 F2 F4 F8 G4 G7 H1 H3 J4 J8

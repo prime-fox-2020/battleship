d67afb6557824686f598e6ec08eb792e044e23a0
akmaliavitri <akmaliavitri21@gmail.com>
2020-04-05 17:28:21 +0700

    baru board

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Your code here
+const input = process.argv;
+
+function board() {
+    let alfabet = 'ABCDEFGHIJ'
+    let boards = []
+
+    for(let row=0; row<=10; row++) {
+        let rows = []
+
+        for(let col=0; col<=10; col++) {
+            if(row === 0) {
+                if(col === 0) {
+                    rows.push(' ')
+                } else {
+                    rows.push(col)
+                }
+            } else if(row !== 0 && col !== 0) {
+                rows.push(alfabet[row-1])
+            } else {
+                rows.push(' ')
+            }
+        }
+        boards.push(rows)
+    }
+    return boards
+}
+console.table(board())
+
